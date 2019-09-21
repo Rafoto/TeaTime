718ff1c96ebaf518bb5427360f0e0d1b6e564c89
Chibueze Ejiofor <ejioforchibu@gmail.com>
2019-09-21 11:44:44 -0500

    Initialized some fragments

--- app/src/main/java/com/example/teatime/MainActivity.java
+++ app/src/main/java/com/example/teatime/MainActivity.java
@@ -4,16 +4,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Map;
 
+public class MainActivity extends AppCompatActivity {
+    private static final int ACCESS_FINE_LOCATION_REQUEST_READ_CONTACTS = 56;
     private DrawerLayout mDrawer;
     private Toolbar toolbar;
     private NavigationView nvDrawer;
@@ -35,8 +43,40 @@ public class MainActivity extends AppCompatActivity {
 
         // Find our drawer view
         mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        setupDrawerContent(mDrawer);
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+        // Setup drawer view
+        setupDrawerContent(nvDrawer);
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    ACCESS_FINE_LOCATION_REQUEST_READ_CONTACTS);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case ACCESS_FINE_LOCATION_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // locatinos-related task you need to do.
+                } else {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            ACCESS_FINE_LOCATION_REQUEST_READ_CONTACTS);
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 
     private void setupDrawerContent(NavigationView navigationView) {
@@ -50,6 +90,43 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+    Fragment fragment = null;
+    Class fragmentClass;
+        switch(menuItem.getItemId()) {
+        case R.id.map:
+            fragmentClass = MapFragment.class;
+            break;
+        case R.id.profile:
+            fragmentClass = ProfileFragment.class;
+            break;
+        case R.id.addDrop:
+            fragmentClass = AddDropFragment.class;
+            break;
+        default:
+            fragmentClass = MapFragment.class;
+    }
+
+        try {
+        fragment = (Fragment) fragmentClass.newInstance();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+
+    // Insert the fragment by replacing any existing fragment
+    FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+    // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+    // Set action bar title
+    setTitle(menuItem.getTitle());
+    // Close the navigation drawer
+        mDrawer.closeDrawers();
+
+    }
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -60,7 +137,6 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
-
 
     }
 }
